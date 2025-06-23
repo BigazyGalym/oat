1f4ab61821bb9f9c190662084e087316086f99f9
Галым Бигазы <galymbigazy@MacBook-Air-Galym.local>
2025-06-23 22:40:37 +0500

    Fix: Add Render domain to ALLOWED_HOSTS

--- online_agro_technics/online_agro_technics/settings.py
+++ online_agro_technics/online_agro_technics/settings.py
@@ -9,17 +9,18 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+DEBUG = 'RENDER' not in os.environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-SECRET_KEY = 'django-insecure-your-secret-key-here'  # Өз кілтіңізді қосыңыз
+SECRET_KEY = '3u0-cnz)!f)72o67@r@_&-4-5!phx$6_q+7y(=u#zf!mrhfhq5'  # Өз кілтіңізді қосыңыз
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','https://server-production-0e96.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','https://server-production-0e96.up.railway.app','https://online-agro-technic-service.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
